5989ecf9b95ee330ec0f8f62534e2ca9778c712a
IRBentz <168255936+IRBentz@users.noreply.github.com>
2025-07-27 15:32:13 -0400

    JSON Import Testing

--- src/com/engine/JsonKeyValues.java
+++ src/com/engine/JsonKeyValues.java
@@ -1,6 +1,6 @@
 package com.engine;
 
 public enum JsonKeyValues {
-	attack, card_type, cards, defense, icon, index, level, linkRating, lore, monsterAttribute, monsterType, name, pendulumLevel,
-	pendulumLore, rank, summonRequirment, type;
+	attack, card_type, cards, defense, icon, index, level, linkArrow, linkRating, lore, monsterAttribute, monsterType, name, pendulumLevel,
+	pendulumLore, rank, summonRequirement, type;
 }
--- src/com/testing/JsonImporting.java
+++ src/com/testing/JsonImporting.java
@@ -1,6 +1,25 @@
 package com.testing;
 
 import static com.engine.ClassPathEnum.Component;
+import static com.engine.Global.card_db;
+import static com.engine.JsonKeyValues.attack;
+import static com.engine.JsonKeyValues.card_type;
+import static com.engine.JsonKeyValues.cards;
+import static com.engine.JsonKeyValues.defense;
+import static com.engine.JsonKeyValues.icon;
+import static com.engine.JsonKeyValues.index;
+import static com.engine.JsonKeyValues.level;
+import static com.engine.JsonKeyValues.linkArrow;
+import static com.engine.JsonKeyValues.linkRating;
+import static com.engine.JsonKeyValues.lore;
+import static com.engine.JsonKeyValues.monsterAttribute;
+import static com.engine.JsonKeyValues.monsterType;
+import static com.engine.JsonKeyValues.name;
+import static com.engine.JsonKeyValues.pendulumLevel;
+import static com.engine.JsonKeyValues.pendulumLore;
+import static com.engine.JsonKeyValues.rank;
+import static com.engine.JsonKeyValues.summonRequirement;
+import static com.engine.JsonKeyValues.type;
 
 import java.io.IOException;
 import java.nio.file.FileSystems;
@@ -11,10 +30,18 @@ import java.util.ArrayList;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import com.card.ExtraMonCard;
+import com.card.LinkMonCard;
 import com.card.MonCard;
+import com.card.PenMonCard;
+import com.card.SpellCard;
+import com.card.TrapCard;
+import com.card.XyzMonCard;
 import com.card.component.CardComponent;
 import com.card.component.CardComponentInterface;
 import com.card.component.CardSubcomponentInterface;
+import com.card.component.IconComponent;
+import com.card.component.LinkArrowComponent;
 import com.card.component.MonsterAttributeComponent;
 import com.card.component.MonsterTypeComponent;
 import com.card.component.TypeComponent;
@@ -25,22 +52,98 @@ public class JsonImporting {
 
 	public static void main(String[] args) {
 
-		Path filePath = FileSystems.getDefault().getPath("src/com/db/_json/monster.json");
+		Path filePath = FileSystems.getDefault().getPath("src/com/db/_json/card.json");
 		try {
 			JSONArray jsonArray = new JSONArray(Utils.concatStringArray(Files.readAllLines(filePath)));
-			for(Object obj : jsonArray) {
+			for (Object obj : jsonArray) {
 				JSONObject jObject = (JSONObject) obj;
-				Global.card_db.add(new MonCard(
-						jObject.getString("name"),
-						jObject.getInt("index"),
-						stringConvertCCI(jObject.getString("monsterAttribute")).castTo((MonsterAttributeComponent.class)),
-						stringConvertCCI(jObject.getString("monsterType")).castTo(MonsterTypeComponent.class),
-						stringToCCIArrayList(jObject.getJSONArray("type")).toArray(TypeComponent[]::new),
-						jObject.getString("lore"),
-						jObject.getInt("level"),
-						jObject.getInt("attack"),
-						jObject.getInt("defense")
-						));
+				String card_typeName = jObject.getString(card_type.name());
+				if (card_typeName.equals("monster")) {
+					for (Object obj2 : jObject.getJSONArray(cards.name())) {
+						JSONObject jObject2 = (JSONObject) obj2;
+						Global.card_db.add(new MonCard(jObject2.getString(name.name()), jObject2.getInt(index.name()),
+								stringConvertCCI(jObject2.getString(monsterAttribute.name()))
+										.castTo((MonsterAttributeComponent.class)),
+								stringConvertCCI(jObject2.getString(monsterType.name()))
+										.castTo(MonsterTypeComponent.class),
+								stringToCCIArrayList(jObject2.getJSONArray(type.name())).toArray(TypeComponent[]::new),
+								jObject2.getString(lore.name()), jObject2.getInt(level.name()),
+								jObject2.getInt(attack.name()), jObject2.getInt(defense.name())));
+					}
+				} else if (card_typeName.equals("pendulumMonster")) {
+					for (Object obj2 : jObject.getJSONArray(cards.name())) {
+						JSONObject jObject2 = (JSONObject) obj2;
+						Global.card_db.add(new PenMonCard(jObject2.getString(name.name()),
+								jObject2.getInt(index.name()),
+								stringConvertCCI(jObject2.getString(monsterAttribute.name()))
+										.castTo((MonsterAttributeComponent.class)),
+								stringConvertCCI(jObject2.getString(monsterType.name()))
+										.castTo(MonsterTypeComponent.class),
+								stringToCCIArrayList(jObject2.getJSONArray(type.name())).toArray(TypeComponent[]::new),
+								jObject2.getString(pendulumLore.name()), jObject2.getString(lore.name()),
+								jObject2.getInt(level.name()), jObject2.getInt(pendulumLevel.name()),
+								jObject2.getInt(attack.name()), jObject2.getInt(defense.name())));
+					}
+				} else if (card_typeName.equals("fusionMonster") || card_typeName.equals("synchroMonster")) {
+					for (Object obj2 : jObject.getJSONArray(cards.name())) {
+						JSONObject jObject2 = (JSONObject) obj2;
+						Global.card_db.add(new ExtraMonCard(jObject2.getString(name.name()),
+								jObject2.getInt(index.name()),
+								stringConvertCCI(jObject2.getString(monsterAttribute.name()))
+										.castTo((MonsterAttributeComponent.class)),
+								stringConvertCCI(jObject2.getString(monsterType.name()))
+										.castTo(MonsterTypeComponent.class),
+								stringToCCIArrayList(jObject2.getJSONArray(type.name())).toArray(TypeComponent[]::new),
+								jObject2.getString(summonRequirement.name()), jObject2.getString(lore.name()),
+								jObject2.getInt(level.name()), jObject2.getInt(attack.name()),
+								jObject2.getInt(defense.name())));
+					}
+				} else if (card_typeName.equals("xyzMonster")) {
+					for (Object obj2 : jObject.getJSONArray(cards.name())) {
+						JSONObject jObject2 = (JSONObject) obj2;
+						Global.card_db.add(new XyzMonCard(jObject2.getString(name.name()),
+								jObject2.getInt(index.name()),
+								stringConvertCCI(jObject2.getString(monsterAttribute.name()))
+										.castTo((MonsterAttributeComponent.class)),
+								stringConvertCCI(jObject2.getString(monsterType.name()))
+										.castTo(MonsterTypeComponent.class),
+								stringToCCIArrayList(jObject2.getJSONArray(type.name())).toArray(TypeComponent[]::new),
+								jObject2.getString(summonRequirement.name()), jObject2.getString(lore.name()),
+								jObject2.getInt(rank.name()), jObject2.getInt(attack.name()),
+								jObject2.getInt(defense.name())));
+					}
+				} else if (card_typeName.equals("linkMonster")) {
+					for (Object obj2 : jObject.getJSONArray(cards.name())) {
+						JSONObject jObject2 = (JSONObject) obj2;
+						Global.card_db.add(new LinkMonCard(jObject2.getString(name.name()),
+								jObject2.getInt(index.name()),
+								stringConvertCCI(jObject2.getString(monsterAttribute.name()))
+										.castTo((MonsterAttributeComponent.class)),
+								stringConvertCCI(jObject2.getString(monsterType.name()))
+										.castTo(MonsterTypeComponent.class),
+								stringToCCIArrayList(jObject2.getJSONArray(type.name())).toArray(TypeComponent[]::new),
+								jObject2.getString(summonRequirement.name()), jObject2.getString(lore.name()),
+								jObject2.getInt(linkRating.name()), jObject2.getInt(attack.name()),
+								stringToCCIArrayList(jObject2.getJSONArray(linkArrow.name()))
+										.toArray(LinkArrowComponent[]::new)));
+					}
+				} else if (card_typeName.equals("spell")) {
+					for (Object obj2 : jObject.getJSONArray(cards.name())) {
+						JSONObject jObject2 = (JSONObject) obj2;
+						card_db.add(new SpellCard(jObject2.getString(name.name()), jObject2.getInt(index.name()),
+								jObject2.getString(lore.name()),
+								stringConvertCCI(jObject2.getString(icon.name())).castTo(IconComponent.class)));
+					}
+				} else if (card_typeName.equals("trap")) {
+					for (Object obj2 : jObject.getJSONArray(cards.name())) {
+						JSONObject jObject2 = (JSONObject) obj2;
+						card_db.add(new TrapCard(jObject2.getString(name.name()), jObject2.getInt(index.name()),
+								jObject2.getString(lore.name()),
+								stringConvertCCI(jObject2.getString(icon.name())).castTo(IconComponent.class)));
+					}
+				} else {
+					System.out.println("Skipped: " + card_typeName);
+				}
 			}
 			System.out.println(Global.card_db);
 		} catch (IOException e) {
