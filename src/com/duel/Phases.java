87a9480e0f03d918a7be13d314b168604b5ead2f
IRBentz <168255936+IRBentz@users.noreply.github.com>
2025-08-02 11:44:42 -0400

    Adding phase tooling for game state management

---
+++ src/com/duel/Phases.java
@@ -0,0 +1,9 @@
+package com.duel;
+
+public enum Phases {
+	Draw, Stand_by, Main, Battle, Main_2, End;
+
+	public enum BattleSteps {
+		Start, Battle, Damage, End;
+	}
+}
---
+++ src/com/testing/Phase_Step_Testing.java
@@ -0,0 +1,8 @@
+package com.testing;
+
+public class Phase_Step_Testing {
+
+	public Phase_Step_Testing() {
+		
+	}
+}
