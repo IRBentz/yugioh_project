30c305d4083bd620e93e747ca5a1801bc00a09a0
IRBentz <168255936+IRBentz@users.noreply.github.com>
2025-08-02 11:44:18 -0400

    Follow up on QueDB Builder removal

--- src/com/engine/Global.java
+++ src/com/engine/Global.java
@@ -6,7 +6,9 @@ import com.card.Card;
 import com.player.Player;
 
 public abstract class Global {
+	@Deprecated
 	public static int back_ver;
+	public static int padding = 15;
 	public static ArrayList<Card> card_db = new ArrayList<>();
 	public static ArrayList<int[]> fal_list = new ArrayList<>();
 	public static Player opponent = new Player();
