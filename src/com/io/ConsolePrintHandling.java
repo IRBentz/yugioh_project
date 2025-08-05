5ee18054b6196e220180173644d0d18a69190953
IRBentz <168255936+IRBentz@users.noreply.github.com>
2025-08-05 19:57:45 -0400

    Additional logging changes

--- src/com/io/ConsolePrintHandling.java
+++ src/com/io/ConsolePrintHandling.java
@@ -1,38 +1,47 @@
 package com.io;
 
+
 import java.util.Arrays;
 
 import com.engine.Global;
 
 public abstract class ConsolePrintHandling {
 	private static int callIndex = 1;
-
+	private static String prevCallClass = "";
+	
+	private ConsolePrintHandling() {}
+	
 	public static void println(Object... stringToPrint) {
 		callIndex++;
-		print_("%-" + Global.padding + "s%s%n", stringToPrint);
+		printfa("%-" + Global.padding + "s%s%n", stringToPrint);
 	}
 
 	public static void print(Object... stringToPrint) {
 		callIndex++;
-		print_("%-" + Global.padding + "s%s", stringToPrint);
+		printfa("%-" + Global.padding + "s%s", stringToPrint);
 	}
 
-	public static void print_(String format, Object... stringToPrint) {
+	public static void printfa(String format, Object... stringToPrint) {
 		callIndex++;
 		printf(format, stringToPrint);
 	}
 
 	public static void printf(String format, Object... stringToPrint) {
 		callIndex++;
+		String callHeader1 = "";
 		try {
-			System.out.printf(format,
-					'[' + Class.forName(Thread.currentThread().getStackTrace()[callIndex].getClassName())
-							.getSimpleName() + ']',
-					new StringBuilder(Arrays.asList(stringToPrint).toString()).replace(0, 1, "").reverse()
-							.replace(0, 1, "").reverse().toString());
+			callHeader1 = '[' + Class.forName(Thread.currentThread().getStackTrace()[callIndex].getClassName()).getSimpleName().strip() + ']';
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		}
+		
+		if(prevCallClass.equals(callHeader1)) {
+			callHeader1 = "|";
+		}
+		prevCallClass = callHeader1;
+		System.out.printf(format, callHeader1,
+				new StringBuilder(Arrays.asList(stringToPrint).toString()).replace(0, 1, "").reverse()
+						.replace(0, 1, "").reverse().toString()).toString();
 		callIndex = 1;
 	}
 }
