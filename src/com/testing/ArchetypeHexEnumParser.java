76cd1ac2ddb124e2ecec5fe3a274e0ebe1110e7a
IRBentz <168255936+IRBentz@users.noreply.github.com>
2025-08-13 17:59:29 -0400

    Spelling cont.

--- src/com/testing/ArchetypeHexEnumParser.java
+++ src/com/testing/ArchetypeHexEnumParser.java
@@ -6,17 +6,17 @@ import java.util.logging.Logger;
 import com.card.component.ArchetypeComponent;
 
 public abstract class ArchetypeHexEnumParser {
-	public static int archtypeHexParseInt(ArchetypeComponent.HexCode hexCode) {
-		return Integer.parseInt(archtypeHexParseString(hexCode), 16);
+	public static int archetypeHexParseInt(ArchetypeComponent.HexCode hexCode) {
+		return Integer.parseInt(archetypeHexParseString(hexCode), 16);
 	}
 	
-	public static String archtypeHexParseString(ArchetypeComponent.HexCode hexCode) {
+	public static String archetypeHexParseString(ArchetypeComponent.HexCode hexCode) {
 		return hexCode.name().substring(4).replace("_", "");
 	}
 	
 	public static void main(String[] args) {
 		for(ArchetypeComponent a : ArchetypeComponent.class.getEnumConstants())
-			Logger.getLogger(ArchetypeHexEnumParser.class.getName()).log(Level.INFO, archtypeHexParseString(a.getHexCode()));
+			Logger.getLogger(ArchetypeHexEnumParser.class.getName()).log(Level.INFO, () -> archetypeHexParseString(a.getHexCode()));
 	}
 
 }
